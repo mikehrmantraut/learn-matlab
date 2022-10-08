272461fb2cea60cf89410a5cc8808c4244d738a4
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-10-08 14:09:21 +0300

    Create matris_islemleri.m

---
+++ matris_islemleri.m
@@ -0,0 +1,33 @@
+%--Matrisler
+% Matris tanımlama
+x = [1 2 3];
+y = [1 2 3; 5 4 1; 2 1 3];
+
+disp(x);
+disp(y);
+
+%--Matris İşlemleri
+% Matrislerde Dört İşlem
+a = [1 2 3 4 5];
+b = a + 2;
+c = a - 2;
+d = a * 2;
+e = a / 2;
+
+disp(a);
+disp(b);
+disp(c);
+disp(d);
+disp(e);
+
+% Matrisle Diğer bir Matris Arasındaki İşlemler
+A = [1 2 3];
+B = [3; 2; 1];
+C = A.*B;
+disp(C);
+
+% Matris transpozu
+k = [1 4 2; 3 2 1];
+
+disp(k);
+disp(k');
